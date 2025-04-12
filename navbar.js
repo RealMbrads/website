cfc97cb96dbc932cebd96c19f3c81f6bdb7c91d8
RealMbrads <65929196+RealMbrads@users.noreply.github.com>
2025-04-12 12:26:36 -0700

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,10 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("nav.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("navbar").innerHTML = data;
-    })
-    .catch((err) => {
-      console.error("Failed to load navigation:", err);
-    });
-});
+<script>
+  window.addEventListener("DOMContentLoaded", () => {
+    fetch("nav.html")
+      .then((res) => res.text())
+      .then((data) => {
+        document.getElementById("navbar").innerHTML = data;
+      });
+  });
+</script>
